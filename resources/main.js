bfa57d80d648c09098cd9ea9320c27872bbb5169
Roxana Popescu <roxanateodorapopescu@yahoo.com>
2021-04-15 15:43:27 +0300

    code review

--- resources/main.js
+++ resources/main.js
@@ -36,28 +36,35 @@ $(function() {
         $(this).addClass('selected');
 
         let categories = $(this).data('product');
-        switch(categories){
-            case 'coats':
-                    console.log(categories);
-                    productContentWrapper.empty();
-                    addProduct(categories);
-                break;
-            case 'dresses':
-                    console.log(categories);
-                    productContentWrapper.empty();
-                    addProduct(categories);
-                break;
-            case 'jersey':
-                    console.log(categories);
-                    productContentWrapper.empty();
-                    addProduct(categories);
-                break;
-            case 'pants':
-                    console.log(categories);
-                    productContentWrapper.empty();
-                    addProduct(categories);
-                break;
-        };
+        // switch(categories){
+        //     case 'coats':
+        //             console.log(categories);
+        //             productContentWrapper.empty();
+        //             addProduct(categories);
+        //         break;
+        //     case 'dresses':
+        //             console.log(categories);
+        //             productContentWrapper.empty();
+        //             addProduct(categories);
+        //         break;
+        //     case 'jersey':
+        //             console.log(categories);
+        //             productContentWrapper.empty();
+        //             addProduct(categories);
+        //         break;
+        //     case 'pants':
+        //             console.log(categories);
+        //             productContentWrapper.empty();
+        //             addProduct(categories);
+        //         break;
+        // };
+
+        // daca incercam sa extragem ce este comun aici, observam ca tot. practic variabila ta categories este legatura cu array-ul de care ai nevoie din products la click pe menu-item-ul respectiv si prin urmare nu ai nevoie de switch, acest switch se intampla de facpt automat pentru ca ai folosit acelasi nume de proprietate in data-products
+        console.log(categories);
+        productContentWrapper.empty();
+        addProduct(categories);
+
+        productContentWrapper.data('selected-category', categories);
     });
 
     const singleImage =$('.product-wrapper-overlay').find('.single-product-overlay');
@@ -65,10 +72,14 @@ $(function() {
 
     productContentWrapper.delegate('.single-product-image', 'click', function(){
         let index = $(this).data('index');
+        let categories = productContentWrapper.data('selected-category');
         let productIndex = products[categories][index];
 
+        //  cateogories l-ai salvat global pe randul 4 si drept urmare este mereu "coats", primul menu-item, practic
+        // cand selectezi un menu-item trebuie sa iti si salvezi selectia asta undeva ca sa o poti recupera mai tarziu pentru context; pe randul 67 eu am salvat pe productContentWrapper
         console.log(categories);
-        console.log($(this).data());
+        console.log(productIndex)
+        // console.log($(this).data());
 
         const overlayDetails = $('.details-overlay');
         singleImage.css({backgroundImage: `url(assets/${$(this).data('img')})`});
